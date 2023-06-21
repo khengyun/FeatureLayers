364dd2fd6b6fcf55a468f38ea5787407ed6e3dd8
khaangnguyeen <78076796+khengyun@users.noreply.github.com>
2023-06-21 20:41:20 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 from setuptools import setup, find_packages
-import setuptools_scm
-
 
 setup(
     name='featurelayers',
     description='featurelayers Package',
     author='khengyun',
-    author_email='khaangnguyeen@gmail.com',
-    packages=find_packages(include=['featurelayers', 'featurelayers.layers']),
+    author_email='khaangnguyeen@email.com',
+    packages=find_packages(),
     install_requires=[
         'numpy',
         'scipy',
         'tensorflow',
         'keras',
         'scipy',
-        'setuptools_scm',
+        'setuptools_scm'
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -23,17 +21,6 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
     ],
-    use_scm_version={
-        'version_scheme': 'post-release',
-        'local_scheme': 'node-and-date',
-        'write_to': 'featurelayers/_version.py',
-        'fallback_version': '0.1.0',
-        'git_describe_command': 'git describe --tags --dirty --always',
-        'tag_regex': r'^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$',
-        'parse': setuptools_scm.parse.parse_version,
-        'write_to_template': '''\
-__version__ = "{version}"
-'''
-    },
+    use_scm_version=True,
     setup_requires=['setuptools_scm'],
 )
