64780ab871068931d1aa2009bd937ed066ebbc0e
khaangnguyeen <78076796+khengyun@users.noreply.github.com>
2023-06-22 00:34:29 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='featurelayers',
-    version="1.0.7",
+    version="1.0.8",
     description='featurelayers Package',
     author='khengyun',
     author_email='khaangnguyeen@email.com',
